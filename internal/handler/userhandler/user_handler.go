cafe40bae69bbebf27fafc3bf5c9020b5149b6e3
Jonathan Oliveira <jojortd0@gmail.com>
2025-02-27 05:07:42 -0300

    feat: add create user validation

--- internal/handler/userhandler/user_handler.go
+++ internal/handler/userhandler/user_handler.go
@@ -1,5 +1,41 @@
 package userHandler
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"net/http"
 
-func (h *handler) CreateUser(w http.ResponseWriter, r *http.Request) {}
+	"github.com/DevJonathanSantos/poc-go-api/internal/dto"
+	httpErr "github.com/DevJonathanSantos/poc-go-api/internal/handler/httperr"
+	"github.com/DevJonathanSantos/poc-go-api/internal/handler/validation"
+)
+
+func (h *handler) CreateUser(w http.ResponseWriter, r *http.Request) {
+	var req dto.CreateUserDto
+
+	if r.Body == http.NoBody {
+		slog.Error("body is empty", slog.String("package", "userHandler"))
+		w.WriteHeader(http.StatusBadRequest)
+		msg := httpErr.NewBadRequestError("body is required")
+		json.NewEncoder(w).Encode(msg)
+		return
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		slog.Error("error to decode body", "err", err, slog.String("package", "userHandler"))
+		w.WriteHeader(http.StatusBadRequest)
+		msg := httpErr.NewBadRequestError("error to decode body")
+		json.NewEncoder(w).Encode(msg)
+		return
+	}
+
+	httpErr := validation.ValidateHttpData(req)
+	if httpErr != nil {
+		slog.Error(fmt.Sprintf("error to validate data: %v", httpErr), slog.String("package", "userHandler"))
+		w.WriteHeader(httpErr.Code)
+		json.NewEncoder(w).Encode(httpErr)
+		return
+	}
+}
